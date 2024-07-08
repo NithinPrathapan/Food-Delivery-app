27d883126c8fc2f72514ce96e223a53231707c74
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-07-08 13:20:37 +0530

    connceting db

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,34 @@
-import express from 'express'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+//app config
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 4000;
+
+// middleware for
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+// conncection to db and port
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("CONNECTED TO DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(port, () => {
+  console.log("connected to port", port);
+});
