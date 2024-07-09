42deced7e1a16676434d07cf7e1bc6812d4af424
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-07-09 12:29:07 +0530

    navlink to add list orders pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
-
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Orders from "./pages/Orders/Orders";
 const App = () => {
   return (
     <div className="">
@@ -9,6 +12,11 @@ const App = () => {
       <hr />
       <div className="flex">
         <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />}></Route>
+          <Route path="/list" element={<List />}></Route>
+          <Route path="/orders" element={<Orders />}></Route>
+        </Routes>
       </div>
     </div>
   );
--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import { assets } from "../../assets/assets.js";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="w-[18%] min-h-screen border-[1.5px] border-[#a9a9a9] border-t-0  text-[10px]">
       <div className="pt-[50px] pl-[20%] flex flex-col gap-[20px]">
-        <div className=" flex items-center gap-[12px] border border-[#a9a9a9] border-r-0 py-2 px-4 rounded-md cursor-pointer rounded-tr-none rounded-br-none">
+        <NavLink
+          to="/add"
+          className=" flex items-center gap-[12px] border border-[#a9a9a9] border-r-0 py-2 px-4 rounded-md cursor-pointer rounded-tr-none rounded-br-none"
+        >
           <img src={assets.add_icon} alt="" />
           <p className="hidden sm:flex">Add items</p>
-        </div>
-        <div className=" flex items-center gap-[12px] border border-[#a9a9a9] border-r-0 py-2 px-4 rounded-md cursor-pointer rounded-tr-none rounded-br-none">
+        </NavLink>
+        <NavLink
+          to="/list"
+          className=" flex items-center gap-[12px] border border-[#a9a9a9] border-r-0 py-2 px-4 rounded-md cursor-pointer rounded-tr-none rounded-br-none"
+        >
           <img src={assets.order_icon} alt="" />
           <p className="hidden sm:flex">List Items</p>
-        </div>
-        <div className=" flex items-center gap-[12px] border border-[#a9a9a9] border-r-0 py-2 px-4 rounded-md cursor-pointer rounded-tr-none rounded-br-none">
+        </NavLink>
+        <NavLink
+          to="/orders"
+          className=" flex items-center gap-[12px] border border-[#a9a9a9] border-r-0 py-2 px-4 rounded-md cursor-pointer rounded-tr-none rounded-br-none"
+        >
           <img src={assets.order_icon} alt="" />
           <p className="hidden sm:flex">Orders</p>
-        </div>
+        </NavLink>
       </div>
     </div>
   );
